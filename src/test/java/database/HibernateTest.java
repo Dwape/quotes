3a9122053b4081df4ebacf1c846e32c39baa167f
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-03-28 10:54:12 -0300

    Tests now work with ID instead of username.

--- src/test/java/database/HibernateTest.java
+++ src/test/java/database/HibernateTest.java
@@ -17,21 +17,27 @@ public class HibernateTest {
         ManageUser manager = new ManageUser();
         //manager.addUser("Giansco", "gianluca.scolaro@ing.austral.edu.ar", "352312", "Gianluca", "Scolaro", new Date(1232332));
         //manager.addUser("Dwape", "eduardo.lalor@ing.austral.edu.ar", "1234323", "Eduardo", "Lalor", new Date(123233221));
-        manager.addUser("Gengu", "noway@mail.com", "afdafa", "Jeff", "Fromtheoverwatchteam", new Date(123434432));
+        User user1 = new User("Gengu", "noway@mail.com", "afdafa", "Jeff", "Fromtheoverwatchteam", new Date(123434432));
+        User user2 = new User("Giansco", "gianluca.scolaro@ing.austral.edu.ar", "352312", "Gianluca", "Scolaro", new Date(1232332));
+        User user3 = new User("Dwape", "eduardo.lalor@ing.austral.edu.ar", "1234323", "Eduardo", "Lalor", new Date(123233221));
+        //manager.addUser(user1);
+        //manager.addUser(user2);
+        manager.addUser(user3);
+
     }
 
     @Test
     public void deleteUserTest(){
         ManageUser manager = new ManageUser();
-        manager.deleteUser("Giansco");
+        manager.deleteUser(1L);
     }
 
     @Test
     public void updateUserTest(){
         ManageUser manager = new ManageUser();
-        manager.changePassword("Giansco", "thisiscool");
-        manager.changeName("Giansco", "Gianni");
-        manager.changeSurname("Giansco", "LeagueofLegends");
-        manager.changeDateOfBirth("Dwape", new Date(122));
+        manager.changePassword(3L, "woninfornite");
+        //manager.changeName("Giansco", "Gianni");
+        //manager.changeSurname("Giansco", "LeagueofLegends");
+        //manager.changeDateOfBirth("Dwape", new Date(122));
     }
 }

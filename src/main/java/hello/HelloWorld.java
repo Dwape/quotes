e45e390fc7398b59d0a6a0c106023c0cd8d72c39
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-03-30 11:30:10 -0300

    Added index.html. Added dependency with java servlets.

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,33 @@
 package hello;
 
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/servlet")
+public class HelloWorld extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        // Do required initialization
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
 }
